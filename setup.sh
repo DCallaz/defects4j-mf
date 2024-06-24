1e26c639315845a95271ad84f3790a0fccf9e74b
Dylan Callaghan <21831599@sun.ac.za>
2024-06-24 13:34:21 +0200

    Fixed setup script and multi script to match

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ while getopts ":h" opt; do
   esac
 done
 shift $((OPTIND -1))
-home="$(dirname $0)/../../"
+home="$(dirname $0)"
 home="$(readlink -f "$(echo ${home/"~"/~})")"
 if [ $# -lt 1 ]; then
   echo "Defects4J directory not given, installing locally..."
@@ -20,7 +20,7 @@ if [ $# -lt 1 ]; then
   export PATH=$PATH:"$home"/defects4j/framework/bin
   d4j_dir="$home/defects4j"
   # check d4j installation
-  $d4j_dir/framework/bin/defects4j info -p Lang &> /dev/null
+  defects4j info -p Lang &> /dev/null
   if [ $? -ne 0 ]; then
     echo "ERROR: Defects4J could not be installed, please install manually."
     exit 1
@@ -30,11 +30,11 @@ else
 fi
 cd "$d4j_dir"
 git apply "$home"/defects4j_multi_with_jars.patch
-$d4j_dir/framework/bin/defects4j_multi -h &> /dev/null
+defects4j_multi -h &> /dev/null
 if [ $? -ne 0 ]; then
   echo "ERROR: Could not set up defects4j-mf properly, exiting..."
   exit 1
 fi
 cd "$home/fault_data"
 tar -xjf multi.tar.bz2
-$d4j_dir/framework/bin/defects4j_multi configure -f "$home/fault_data"
+defects4j_multi configure -f "$home/fault_data"
